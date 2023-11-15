b711ef3ef785aaaa1c763f3307847adf8db05fab
Mauritius Clemens <githubino@janitor.chat>
2023-11-15 19:55:55 +0100

    more learning mir

--- main.cpp
+++ main.cpp
@@ -15,43 +15,47 @@ using namespace miral::toolkit;
 
 int main(int argc, char const* argv[])
 {
-    MirRunner runner{argc, argv};
-	ExternalClientLauncher external_client_launcher;
-	auto const keyboard_shortcuts = [&](MirEvent const* event)
-        {
-			
-            if (mir_event_get_type(event) != mir_event_type_input)
-                return false;
+  MirRunner runner{argc, argv}; // obvious
+  ExternalClientLauncher external_client_launcher;
+  Keymap config_keymap;
+  config_keymap.set_keymap("gb");
+  auto const keyboard_shortcuts = [&](MirEvent const* event)
+  {
+	if (mir_event_get_type(event) != mir_event_type_input)
+	   return false;
+	   
+	MirInputEvent const* input_event = mir_event_get_input_event(event);
+	if (mir_input_event_get_type(input_event) != mir_input_event_type_key)
+       return false;
 
-            MirInputEvent const* input_event = mir_event_get_input_event(event);
-            if (mir_input_event_get_type(input_event) != mir_input_event_type_key)
-                return false;
+    MirKeyboardEvent const* kev = mir_input_event_get_keyboard_event(input_event);
+	if (mir_keyboard_event_action(kev) != mir_keyboard_action_down)
+       return false;
 
-            MirKeyboardEvent const* kev = mir_input_event_get_keyboard_event(input_event);
-            if (mir_keyboard_event_action(kev) != mir_keyboard_action_down)
-                return false;
+    MirInputEventModifiers mods = mir_keyboard_event_modifiers(kev);
+	if (!(mods & mir_input_event_modifier_alt) || !(mods & mir_input_event_modifier_ctrl))
+       return false;
 
-            MirInputEventModifiers mods = mir_keyboard_event_modifiers(kev);
-            if (!(mods & mir_input_event_modifier_alt) || !(mods & mir_input_event_modifier_ctrl))
-                return false;
+	switch (mir_keyboard_event_scan_code(kev))
+    {
+      case KEY_BACKSPACE:
+		   runner.stop();
+           return true;
 
-            switch (mir_keyboard_event_scan_code(kev))
-            {
-            case KEY_BACKSPACE:
-                runner.stop();
-                return true;
+	  case KEY_T:
+           external_client_launcher.launch({std::string("konsole")});
+		   return true;
 
-            case KEY_T:
-                external_client_launcher.launch({std::string("konsole")});
-                return true;
-
-            default:
-                return false;
-            }
-        };
-    return runner.run_with(
-            {
-                    X11Support{},
-                    set_window_management_policy<MinimalWindowManager>()
-            });
+	  default:
+			 return false;
+    }
+  };
+  return runner.run_with(
+  {
+	X11Support{},
+	config_keymap,
+	external_client_launcher,
+	AppendEventFilter(keyboard_shortcuts),
+	set_window_management_policy<MinimalWindowManager>()
+  });
 }
